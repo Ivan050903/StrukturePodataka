a58ee021d97c6bcb53f66f7a5429161e8c7b9635
Ivan050903 <ivanzubac17@gmail.com>
2024-10-08 18:49:57 +0200

    komit3

--- vj1/vj1/Source.c
+++ vj1/vj1/Source.c
@@ -1,6 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#define ERROR_OF -1
+#define BUFFER_SIZE 50;
+
+int count_rows(char* filename);
+
 
 int main() {
-	printf("Hello world!");
+	
+	return 0;
+}
+
+
+int count_rows(char* filename) {
+	FILE* fp = NULL;
+
+	fp = fopen(filename, "r");
+	if (fp == NULL) {
+		printf("Error opening file!\n");
+		return ERROR_OF;
+	}
+
+	while (!feof(fp)) {
+	}
 	return 0;
 }
