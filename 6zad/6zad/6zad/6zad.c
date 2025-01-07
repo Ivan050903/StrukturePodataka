d114e7d678404c116dda263fb7e78a69b4a8ef8f
Ivan050903 <ivanzubac17@gmail.com>
2025-01-07 16:42:53 +0100

    update

--- 6zad/6zad/6zad/6zad.c
+++ 6zad/6zad/6zad/6zad.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -232,7 +232,7 @@ int print_invoice_article(invoiceposition head, int red) {
 		printf("Nema racuna!");
 		return EXIT_FAILURE;
 	}
-	
+
 	for (int i = 0; i < red; i++) {
 		head = head->next;
 		if (head == NULL) {
